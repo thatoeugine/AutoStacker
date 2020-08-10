34eb4364655177769329fcb38103de08f3759eca
Thato Manamela <thatoeugine@gmail.com>
2020-08-10 16:42:51 +0200

    Update Moving_vis_from_model.py

--- stack_auto/Moving_vis_from_model.py
+++ stack_auto/Moving_vis_from_model.py
@@ -19,50 +19,52 @@ import casa
 import numpy as np
 import json
 import numpy.ma as ma
+import ConfigParser
+import sys
+
+if __name__=='__main__':
+    config = ConfigParser.ConfigParser()
+    config.read(sys.argv[-1])
 
 
+    path = config.get('pipeline', 'data_path') 
 
-with open('params.json') as json_data:
-    d = json.load(json_data)
+    #Placing the predicted visibilities from the Model column to Data column
+    casa.uvsub(path+config.get('simulator_params', 'msfile_name'), reverse = True)
+    """
+    tb.open(path+d["Simulator_params"]["msfile_name"].encode("ascii","ignore"),nomodify=False)  
+    corrected_data=tb.getcol("CORRECTED_DATA")
+    data = tb.getcol("DATA")
+    adding_data = corrected_data + data
+    tb.putcol("DATA", adding_data)
+    #tb.putcol("CORRECTED_DATA", adding_data)
 
-path = d["Simulator_params"]["working_directory"].encode("ascii","ignore") 
-
-
-#Placing the predicted visibilities from the Model column to Data column
-casa.uvsub(path+d["Simulator_params"]["msfile_name"].encode("ascii","ignore"), reverse = True)
-"""
-tb.open(path+d["Simulator_params"]["msfile_name"].encode("ascii","ignore"),nomodify=False)  
-corrected_data=tb.getcol("CORRECTED_DATA")
-data = tb.getcol("DATA")
-adding_data = corrected_data + data
-tb.putcol("DATA", adding_data)
-#tb.putcol("CORRECTED_DATA", adding_data)
-
-tb.close()
-"""
+    tb.close()
+    """
 
 
-def uvadd(vis):
-    t = tbtool()
-    casalog.post('Placing the predicted visibilities from the Model column to Data column', 'INFO')
+    def uvadd(vis):
+        t = tbtool()
+        casalog.post('Placing the predicted visibilities from the Model column to Data column', 'INFO')
 
-    t.open(vis, nomodify=False)
-    ram_restrict = 100000
-    ranger = list(range(0,t.nrows(),ram_restrict))
+        t.open(vis, nomodify=False)
+        ram_restrict = 100000
+        ranger = list(range(0,t.nrows(),ram_restrict))
+
+        for colname in ['DATA']:
+            if (colname in t.colnames()) and (t.iscelldefined(colname,0)):
+                for j in ranger:
+                    if j == ranger[-1]:
+                        ram_restrict = t.nrows()%ram_restrict
+                    a = t.getcol('DATA',startrow=j, nrow=ram_restrict, rowincr=1)
+                    corrected_data = t.getcol('CORRECTED_DATA',startrow=j, nrow=ram_restrict, rowincr=1)
+                    #model_mask = model == 0
+                    #model = ma.array(data=model,mask=model_mask)
+                    ### note that the use of np.isin cannot be used in CASA as it still uses numpy version v.1.11 whereas this came in v1.14
+                    ### Had to use old horrible version (see line 51)
+                    a = a+corrected_data
+                    t.putcol('DATA',a,startrow=j, nrow=ram_restrict, rowincr=1)
+
+
+    uvadd(path+config.get('simulator_params', 'msfile_name'))
     
-    for colname in ['DATA']:
-        if (colname in t.colnames()) and (t.iscelldefined(colname,0)):
-            for j in ranger:
-                if j == ranger[-1]:
-                    ram_restrict = t.nrows()%ram_restrict
-                a = t.getcol('DATA',startrow=j, nrow=ram_restrict, rowincr=1)
-                corrected_data = t.getcol('CORRECTED_DATA',startrow=j, nrow=ram_restrict, rowincr=1)
-                #model_mask = model == 0
-                #model = ma.array(data=model,mask=model_mask)
-                ### note that the use of np.isin cannot be used in CASA as it still uses numpy version v.1.11 whereas this came in v1.14
-                ### Had to use old horrible version (see line 51)
-                a = a+corrected_data
-                t.putcol('DATA',a,startrow=j, nrow=ram_restrict, rowincr=1)
-                
-                
-uvadd(path+d["Simulator_params"]["msfile_name"].encode("ascii","ignore"))
