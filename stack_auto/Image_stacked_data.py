d21d9d1253316cde50c8a9103edee54e2f4aae03
Thato Manamela <thatoeugine@gmail.com>
2020-08-10 16:38:10 +0200

    Update Image_stacked_data.py

--- stack_auto/Image_stacked_data.py
+++ stack_auto/Image_stacked_data.py
@@ -16,23 +16,21 @@ Uinversity of Pretoria, Department of Physics, Asronomy Research Group.
 
 import casa
 import numpy as np
-import json
+import ConfigParser
+import sys
 
-
-#Loading the parameters json file:
-
-with open('params.json') as json_data:
-    d = json.load(json_data)
+if __name__=='__main__':
+    config = ConfigParser.ConfigParser()
+    config.read(sys.argv[-1])
     
-path = d["Simulator_params"]["working_directory"].encode("ascii","ignore") 
+    path = config.get('pipeline', 'data_path')
+    stampsize = config.getint('casa_imager', 'stampsize')
+    cell_ = config.get('casa_imager', 'cell')
+    vis_ = config.get('casa_imager', 'stack_msfile_name')
 
-stampsize = d["Imaging_params"]["stampsize"]
-cell_ = d["Imaging_params"]["cell"].encode("ascii","ignore")
-vis_ = d["Imaging_params"]["stack_msfile_name"].encode("ascii","ignore")
+    # Image the uv-stacked data to produce an image.
+    casa.clean(vis=path+vis_, imagename=path+'uvstacked',cell = cell_,\
+               imsize = stampsize, mask = [int(stampsize/2)-2, int(stampsize/2)-2,
+                  int(stampsize/2)+2, int(stampsize/2)+2])
 
-# Image the uv-stacked data to produce an image.
-casa.clean(vis=path+vis_, imagename=path+'uvstacked',cell = cell_,\
-           imsize = stampsize, mask = [int(stampsize/2)-2, int(stampsize/2)-2,
-              int(stampsize/2)+2, int(stampsize/2)+2])
-
-casa.exportfits(path+'uvstacked.image', path+'uvstacked.fits') #exporting casa image to fts
+    casa.exportfits(path+'uvstacked.image', path+'uvstacked.fits') #exporting casa image to fts
