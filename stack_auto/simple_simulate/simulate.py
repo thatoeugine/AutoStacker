47e86d5f042f85b96cabf89728a533185fc11739
Thato Manamela <thatoeugine@gmail.com>
2020-08-10 16:35:40 +0200

    Update simulate.py

--- stack_auto/simple_simulate/simulate.py
+++ stack_auto/simple_simulate/simulate.py
@@ -17,34 +17,48 @@ This module simulate TRECS sources with MeerKAT.
 
 
 import os
-import json
+import configparser
+import sys
+
+if __name__=='__main__':
+    config = configparser.ConfigParser()
+    config.read(sys.argv[-1])
+        
+    path = config.get('pipeline', 'data_path') 
+    
+    print "BEGINNING SIMULATION............................"
+    
+    # creating empty MS table:
+    msoutname = path+config.get('simulator_params', 'msfile_name')
+    ra0_deg = config.getfloat('simulator_params', 'ra_deg0')
+    dec0_deg = config.getfloat('simulator_params', 'dec_deg0')
+    obs_length = config.getint('simulator_params', 'obs_length_hr')
+    dt = config.getint('simulator_params', 'dtime_s')
+    freq0 = config.get('simulator_params', 'freq0_MHz')
+    dfreq = config.get('simulator_params', 'dfreq_MHz')
+    nchan = config.getint('simulator_params', 'nchan')
+    
+    simms_cmd = 'simms -T MeerKAT -t ascii -cs itrf \
+                       -n {0} \
+                       -ra {1}deg \
+                       -dec {2}deg \
+                       -pl "XX XY YX YY" \
+                       -st {3} \
+                       -dt {4} \
+                       -f0 {5} \
+                       -df {6} \
+                       -nc {7} stack_auto/simple_simulate/meerkat.itrf.txt'.format(msoutname,
+                       ra0_deg, dec0_deg,obs_length,dt,freq0,dfreq,nchan)
+    os.system(simms_cmd) # this creates an EMPTY Measurement Set
+    
+    
+    # now simulate an observation of a fake sky model you've created.
+    skymodelname = "stack_auto/simple_simulate/point_src.txt"
+    # The following will write the visiblities into the MODEL column of the empty MS you created. 
+    meqtrees_cmd = 'meqtree-pipeliner.py --mt 32 \-c stack_auto/simple_simulate/tdlconf.profiles [turbo-sim] tiggerlsm.filename=%s ms_sel.msname=%s ms_sel.tile_size=1000000 ms_sel.output_column=DATA stack_auto/simple_simulate/turbo-sim.py =_simulate_MS'%(skymodelname,msoutname)
+    print(meqtrees_cmd)
+    os.system(meqtrees_cmd)
+    
+print "SIMULATION COMPLETE,"
 
 
-######### loading json parameters######
-with open('params.json') as json_data:
-    d = json.load(json_data)
-
-path = d["Simulator_params"]["working_directory"].encode("ascii","ignore") 
-
-print "BEGINNING SIMULATION............................"
-
-# creating empty MS table:
-msoutname = path+d["Simulator_params"]["msfile_name"].encode("ascii","ignore")
-ra0_deg = d["Simulator_params"]["ra_deg0"]
-dec0_deg = d["Simulator_params"]["dec_deg0"]
-obs_length = d["Simulator_params"]["obs_length_hr"]
-dt = d["Simulator_params"]["dtime_s"]
-freq0 = d["Simulator_params"]["freq0_MHz"].encode("ascii","ignore")
-dfreq = d["Simulator_params"]["dfreq_MHz"].encode("ascii","ignore")
-nchan = d["Simulator_params"]["nchan"]
-
-simms_cmd = 'simms -T MeerKAT -t ascii -cs itrf -n %s -ra %sdeg -dec %sdeg -pl "XX XY YX YY" -st %i -dt %i -f0 %s -df %s -nc %i stack_auto/simple_simulate/meerkat.itrf.txt'%(msoutname, ra0_deg, dec0_deg,obs_length,dt,freq0,dfreq,nchan)
-os.system(simms_cmd) # this creates an EMPTY Measurement Set
-
-
-# now simulate an observation of a fake sky model you've created.
-skymodelname = "stack_auto/simple_simulate/point_src.txt"
-# The following will write the visiblities into the MODEL column of the empty MS you created. 
-meqtrees_cmd = 'meqtree-pipeliner.py --mt 32 -c stack_auto/simple_simulate/tdlconf.profiles [turbo-sim] tiggerlsm.filename=%s ms_sel.msname=%s ms_sel.tile_size=1000000 ms_sel.output_column=DATA stack_auto/simple_simulate/turbo-sim.py =_simulate_MS'%(skymodelname,msoutname)
-print(meqtrees_cmd)
-os.system(meqtrees_cmd)
