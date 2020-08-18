eb03b0d4011091e8714d0a9c3948560cb70eae5b
Thato Manamela <thatoeugine@gmail.com>
2020-08-18 13:44:12 +0200

    Update Mastercode.py

--- Mastercode.py
+++ Mastercode.py
@@ -19,12 +19,12 @@ uvstacked_ms = config.get('casa_imager', 'stack_msfile_name')
 original_ms = config.get('simulator_params', 'msfile_name')
 
 skymodel= config.get('pipeline', 'project_name')+'_truthcat.txt'
-skymodel_stacking_depth = config.get('stacking_params', 'stacking_depth_skymodel_name')
 modelimage_ = config.get('field', 'fitsname')
 modelimage = modelimage_.replace("-model.fits"," ")
 #======================================================================================================
 #                                     Simulating with simms+meqtrees+Simuclass
 #======================================================================================================
+from stack_auto.Stacker import runFoVcut, runImstack, runImageStackData, runStack_clean, runMoveVis, runPlotStackingResults, runPlotStackingImages, runStackingDepth, runStackingDepthProto, runUVStack
 
 if config.getboolean('pipeline', 'dosimulate') == True:
     from stack_auto.simple_simulate.Simulator import runSimulate, runAddNoise
@@ -32,41 +32,41 @@ if config.getboolean('pipeline', 'dosimulate') == True:
     runAddNoise(config, sys.argv[1])
 
 
-from stack_auto.Stacker import runFoVcut, runImstack, runImageStackData, runStack_clean, runMoveVis, runPlotStackingResults, runPlotStackingImages, runStackingDepth, runStackingDepthProto, runUVStack
-
-if config.getboolean('stacking_params', 'No._of_srcs_of_choice') == True:
-    runStackingDepthProto(config, sys.argv[1])
-    os.system(KERN3_CONTAINER+' '+'python simuclass/simuCLASS.py'+' '+sys.argv[1])
-    os.system(SOURCE_FINDING_CONTAINER+' '+'python simuclass/Edit_to_Tigger_frendly.py'+' '+PATH+skymodel)
-    
-    ####
-    #Predicting the visibilities
-    ####
-    os.system(SOURCE_FINDING_CONTAINER+' '+'wsclean -mem 100 -predict -name'+' '+PATH+modelimage+' '+PATH+original_ms)
-
-if config.getboolean('stacking_params', 'stacking_depth') == True:
-    runStackingDepth(config, sys.argv[1])
-    os.system(KERN3_CONTAINER+' '+'python simuclass/simuCLASS.py'+' '+sys.argv[1])
-    os.system(SOURCE_FINDING_CONTAINER+' '+'python simuclass/Edit_to_Tigger_frendly.py'+' '+PATH+skymodel)
-    
-    ####
-    #Predicting the visibilities
-    ####
-    os.system(SOURCE_FINDING_CONTAINER+' '+'wsclean -mem 100 -predict -name'+' '+PATH+modelimage+' '+PATH+original_ms)
-    
-
-else:
-    os.system(KERN3_CONTAINER+' '+'python simuclass/simuCLASS.py'+' '+sys.argv[1])
-    os.system(SOURCE_FINDING_CONTAINER+' '+'python simuclass/Edit_to_Tigger_frendly.py'+' '+skymodel_stacking_depth)
-    
-    ####
-    #Predicting the visibilities
-    ####
-    os.system(SOURCE_FINDING_CONTAINER+' '+'wsclean -mem 100 -predict -name'+' '+PATH+modelimage+' '+PATH+original_ms)
 
 
-runMoveVis(config, sys.argv[1])
-          
+    if config.getboolean('stacking_params', 'No._of_srcs_of_choice') == True:
+        runStackingDepthProto(config, sys.argv[1])
+        os.system(KERN3_CONTAINER+' '+'python simuclass/simuCLASS.py'+' '+sys.argv[1])
+        os.system(SOURCE_FINDING_CONTAINER+' '+'python simuclass/Edit_to_Tigger_frendly.py'+' '+PATH+skymodel)
+
+        ####
+        #Predicting the visibilities
+        ####
+        os.system(SOURCE_FINDING_CONTAINER+' '+'wsclean -mem 100 -predict -name'+' '+PATH+modelimage+' '+PATH+original_ms)
+
+    if config.getboolean('stacking_params', 'stacking_depth') == True:
+        runStackingDepth(config, sys.argv[1])
+        os.system(KERN3_CONTAINER+' '+'python simuclass/simuCLASS.py'+' '+sys.argv[1])
+        os.system(SOURCE_FINDING_CONTAINER+' '+'python simuclass/Edit_to_Tigger_frendly.py'+' '+PATH+skymodel)
+
+        ####
+        #Predicting the visibilities
+        ####
+        os.system(SOURCE_FINDING_CONTAINER+' '+'wsclean -mem 100 -predict -name'+' '+PATH+modelimage+' '+PATH+original_ms)
+
+
+    else:
+        os.system(KERN3_CONTAINER+' '+'python simuclass/simuCLASS.py'+' '+sys.argv[1])
+        os.system(SOURCE_FINDING_CONTAINER+' '+'python simuclass/Edit_to_Tigger_frendly.py'+' '+PATH+skymodel)
+
+        ####
+        #Predicting the visibilities
+        ####
+        os.system(SOURCE_FINDING_CONTAINER+' '+'wsclean -mem 100 -predict -name'+' '+PATH+modelimage+' '+PATH+original_ms)
+
+
+    runMoveVis(config, sys.argv[1])
+
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #                                       Stacking
@@ -100,4 +100,4 @@ if config.getboolean('pipeline', 'dostacking') == True:
     runPlotStackingResults(config, sys.argv[1])
 
 else:
-    'Simulation Complete'
+    'All Tasks Complete'
